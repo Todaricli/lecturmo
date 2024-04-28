c9eb7891852daa68f2262176ed6a42049251f3c8
Alden Maiyor <amai111@aucklanduni.ac.nz>
2024-04-29 09:09:03 +1200

    do the data and scheme and lecturer api

---
+++ frontend/src/pages/LecturerPage/LecturerPage.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+
+const LecturerPage = () => {
+    const [coursesList, setCoursesList] = useState(undefined)
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        setIsLoading(true)
+        axios.get(`http://localhost:3000/api/lecturer-page/`).then((list) => {
+            setCoursesList(list.data)
+        }).then(() => { setIsLoading(false) })
+    }, [])
+
+    useEffect(() => {
+        if (coursesList != undefined) {
+
+        }
+    }, [coursesList])
+
+    // const courseElement =() => {
+    //     coursesList.map((course) => {
+    //         <div>
+    //             <p>{course.courseName}</p>
+    //             <p>{course._id}</p>
+    //             <p>{course.level}</p>
+    //         </div>
+    //     })
+    // }
+
+    return (
+        <div>
+            <h1>this is lecturer page</h1>
+            {coursesList&&
+            {coursesList}
+            }
+            
+        </div>
+    )
+}
+
+export default LecturerPage
