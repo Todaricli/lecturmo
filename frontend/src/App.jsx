82318e907538417536757db7c81dbca009bc6497
CarinaQi <qiyue1116@gmail.com>
2024-04-30 15:01:42 +1200

    create login and signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import PageLayout from './layouts/PageLayout';
 import HomePage from './pages/HomePage/HomePage'
 import LoginPage from "./pages/LoginPage/LoginPage";
+import Login from './pages/LoginPage/Login';
+import Signup from './pages/SignupPage/Signup';
 
 function App() {
 
@@ -11,7 +13,8 @@ function App() {
       <Route path="/" element={<PageLayout />}>
         <Route index element={<Navigate to="home" replace />} />
         <Route path="home" element={<HomePage />} />
-        <Route path="login" element={<LoginPage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
       </Route>
     </Routes>
   )
