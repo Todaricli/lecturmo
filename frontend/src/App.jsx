ffa24297c674b034942138e654a01e651249e31c
Todaricli <leetony347@yahoo.com>
2024-05-01 00:23:12 +1200

    add a page skeleton to show how frontend should handle verify email

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import PageLayout from './layouts/PageLayout';
 import HomePage from './pages/HomePage/HomePage'
 import LoginPage from "./pages/LoginPage/LoginPage";
+import VerifyEmail from "./pages/VerifyEmail";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Route index element={<Navigate to="home" replace />} />
         <Route path="home" element={<HomePage />} />
         <Route path="login" element={<LoginPage />} />
+        <Route path="/verifyEmail" element={<VerifyEmail/>}/>
       </Route>
     </Routes>
   )
