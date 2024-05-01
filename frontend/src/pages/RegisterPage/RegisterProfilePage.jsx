44b9d73d401decbfc075ea337d1ae6b0ca232d90
CarinaQi <qiyue1116@gmail.com>
2024-05-01 14:00:02 +1200

    fix route bug

--- frontend/src/pages/RegisterPage/RegisterProfilePage.jsx
+++ frontend/src/pages/RegisterPage/RegisterProfilePage.jsx
@@ -143,7 +143,7 @@ export default function RegisterProfilePage() {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="login" variant="body2">
+                <Link href="/login" variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
