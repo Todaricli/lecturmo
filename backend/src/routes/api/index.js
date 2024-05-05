98e4288d77092b60b4492e2dfc95cf6c8e6bea42
alden maiyor <amai111@aucklanduni.ac.nz>
2024-05-05 20:10:56 +1200

    added searcgbar

--- backend/src/routes/api/index.js
+++ backend/src/routes/api/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 import auth from './authRoutes.js';
 import QrRouters from './QrCode-routes.js';
 import LectureRouter from './lectureRouter.js';
+import SearchRouter from './searchRouters.js';
 
 const router = express.Router();
 
 router.use('/', auth);
 router.use('/', QrRouters);
 router.use('/', LectureRouter)
+router.use('/', SearchRouter)
 
 export default router;
