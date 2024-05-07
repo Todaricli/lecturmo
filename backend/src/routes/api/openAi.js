d32b27d6d716b4539f997bd79d82045d868b2f58
Todaricli <leetony347@yahoo.com>
2024-05-07 14:29:14 +1200

    complete Ai summarize reviews

--- backend/src/routes/api/openAi.js
+++ backend/src/routes/api/openAi.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 import OpenAI from "openai";
 import express from 'express';
+import { Course } from '../../schemas/courseSchema.js';
 
 const router = express.Router();
 const API_KEY = process.env.OPEN_AI_API_KEY ?? '';
@@ -18,22 +19,28 @@ export const openai = new OpenAI({
 
 router.get('/summarizeReview', async (req, res) => {
 
-    const courseName = JSON.stringify(req.body.courseName)
-    const reviewArray = JSON.stringify(req.body.reviewArray);
+    const courseDummyId = req.body.courseId;
 
-    if (!reviewArray) return res.status(400).json("Please provided a review array")
-
-    let completion;
+    if(!courseDummyId) return res.status(400).json("Please provided a course you want to get summary");
 
     try {
-        completion = await openai.chat.completions.create({
+        const course = await Course.findOne({ course_dummy_id: courseDummyId });
+
+        if (!course) return res.status(500).json("Unable to retrieve course reviews data")
+
+        const courseName = JSON.stringify(course.course_dummy_id + ': ' + course.courseName)
+        const reviewArray = JSON.stringify(course.reviews);
+
+        if (!reviewArray) return res.status(400).json("This course has no review yet!")
+
+        const completion = await openai.chat.completions.create({
             messages: [{ role: "user", content: `Please act like a geniue course advisor summarize the reviews into only 50 words paragraph for course ${courseName} provided below in plain languages: ${reviewArray} for prospective students who might want to take the course and provide a rating 1 out of 5` }],
             model: "gpt-3.5-turbo",
         });
 
         if (!completion) return res.status(500).json("Unable to generate summary, please try again in 10 minutes!");
 
-        return res.status(200).json(completion.choices[0])
+        return res.status(200).json(completion.choices[0].message.content)
 
     } catch (e) {
         console.log(e.messages)
