8be54394f32d3cf0b2e16b6210d4307607133c47
tonexie <tone.xie@gmail.com>
2024-05-08 15:06:09 +1200

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    sourcemap: true, // or "inline" to include the source map directly in the output files
   },
   plugins: [react()],
 });
