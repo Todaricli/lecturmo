4fe64efa6d1d7c9b406081a6b962f39a6d261879
tonexie <tone.xie@gmail.com>
2024-05-09 20:58:26 +1200

    update

--- backend/src/middleware/authMW.js
+++ backend/src/middleware/authMW.js
@@ -1,9 +1,9 @@
 export const authenticate = (req, res, next) => {
-  console.log("Session ID:", req.sessionID); // Log the session ID
-  console.log("Session:", req.session); // Log the session content
-  console.log("Raw Cookie Header:", req.headers.cookie);
-  console.log("Cookies:", req.cookies); // Log cookies sent by the browser
-  console.log("User:", req.user); // Log user information stored in session
+  // console.log("Session ID:", req.sessionID); // Log the session ID
+  // console.log("Session:", req.session); // Log the session content
+  // console.log("Raw Cookie Header:", req.headers.cookie);
+  // console.log("Cookies:", req.cookies); // Log cookies sent by the browser
+  // console.log("User:", req.user); // Log user information stored in session
   if (req.isAuthenticated()) {
     return next();
   }
